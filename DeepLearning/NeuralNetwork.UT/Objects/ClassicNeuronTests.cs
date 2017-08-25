d503711c4641407a113667d690e17023aa79d831
Sartek-Moloc <kontonacrap@gmail.com>
2017-08-25 19:50:36 +0200

    ClassicNeuron test added

---
+++ DeepLearning/NeuralNetwork.UT/Objects/ClassicNeuronTests.cs
@@ -0,0 +1,27 @@
+﻿using NeuralNetwork.Objects.Implementations;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace NeuralNetwork.UT.Objects
+{
+    [TestFixture]
+    public class ClassicNeuronTests
+    {
+        [Test]
+        public void SimpleTest()
+        {
+            //given
+            List<double> weigts = new List<double> { 1.0, 1.0 };
+            ClassicNeuron neuron = new ClassicNeuron() {Weight = weigts };
+            List<double> input = new List<double> { 1, 1 };
+            double bias = 1;
+
+            //when
+            double result = neuron.Compute(bias,input);
+
+            //then
+            Assert.AreEqual(3.0, result);
+
+        }
+    }
+}
